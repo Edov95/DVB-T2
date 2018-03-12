885ac39168f2257363109d03e75ba09f178a80dc
Edoardo Vanin <edoardo.vanin@gmail.com>
2018-03-12 23:14:31 +0100

    Added a new function file with the updated functionality for generating the B matrix and updated the matrix_generation file

---
+++ generate_B_matrix.m
@@ -0,0 +1,37 @@
+function [B] = generate_B_matrix(rate,N_ldpc)
+%GENERATE_B_MATRIX Summary of this function goes here
+%   Given the code rate and the lenght of the word return the matrix B
+%   for that code
+
+    K_ldpc = N_ldpc * rate; % length of the information word
+    parity_length = N_ldpc - K_ldpc; % length of the parity bits
+
+
+    % Open the related file
+    % need to use a switch condition to choose the correct code
+    fid = fopen('./codes/64800/rate_23');
+
+    % Create the systematic matrix
+    B = sparse(N_ldpc-K_ldpc,K_ldpc);
+
+    % Add the identity in the first block
+    %G = sparse(1:32400,1:32400,1);
+
+    % Block counter
+    n_block = 0;
+
+    % Populate the matrix
+    while ~feof(fid)
+         x = sscanf(fgetl(fid),'%f');
+         for i = 1:360
+             B(x + 1, n_block*360 + i) = 1;
+             x = mod(x + i*60, parity_length);
+             %disp(i.');
+         end
+         n_block = n_block + 1;
+    end
+
+    fclose(fid);
+
+end
+
--- matrix_generator.m
+++ matrix_generator.m
@@ -1,29 +1,20 @@
 % Code that construct the generating matrix of the LDPC code
 
-clear all; close all;
+clear all; close all; clc;
 
-% Open the related file
-fid = fopen('./codes/64800/rate_12');
+rate = 2/3;
+N_ldpc = 64800;
+K_ldpc = 64800*rate;
 
-% Create the systematic matrix
-G = sparse(64800,34200);
+x = rand(K_ldpc,1);
+x(find(x >= 0.5)) = 1;
+x(find(x < 0.5)) = 0;
 
-% Add the identity in the first block
-G = sparse(1:34200,1:34200,1);
+B = generate_B_matrix(rate,N_ldpc);
 
-% Block counter
-n_block = 0;
+parity = B*x;
+parity = cumsum(parity);
+parity = mod(parity,2);
 
-% Populate the matrix
-while ~feof(fid)
-     x = sscanf(fgetl(fid),'%f');
-     for i = 1:360
-         G(34200 + x + 1, n_block*360 + i) = 1;
-         x = mod(x + i*90, 34200);
-         %disp(i.');
-     end
-     n_block = n_block + 1;
-end
 
-fclose(fid);
 
